854b70ce3b7afed5c5a3907281afc1c264e7a6ad
Th-429B <tenghonlau@gmail.com>
2022-03-04 09:15:05 +0000

    Add testcases

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,11 @@
 class StaticPagesController < ApplicationController
+ #since it inherits from application controller, the empty method will map to {methodname}.html.erb view
   def home
   end
 
   def help
+  end
+  
+  def about
   end
 end
