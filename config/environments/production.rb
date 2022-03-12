8a20d20e56121a7720325a60e56777e97a908a24
Th-429B <tenghonlau@gmail.com>
2022-03-12 10:50:43 +0000

    Fix heroku host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'powerful-hollows-50123.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  Rails.application.routes.default_url_options[:host] = "powerful-hollows-50123.herokuapp.com"
+
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
